094db9479eb4b4f2005e4ad0fbf91f21220d7ecd
carolliie <anacvieira1415@gmail.com>
2024-02-29 14:43:28 -0300

    package update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const currentYear = new Date().getFullYear();
 function Footer() {
     return (
         <footer>
-            <p>Copyright Carile© {currentYear}</p>
+            <p>Copyright Carile © {currentYear}</p>
         </footer>
     )  
 }
