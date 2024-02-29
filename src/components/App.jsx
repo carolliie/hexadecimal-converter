25906ed5f68e9c413d193ffb77f177b98fd6c0ee
carolliie <anacvieira1415@gmail.com>
2024-02-29 13:27:22 -0300

    insert files

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Form from "./Form";
+import Footer from "./Footer";
+
+var userIsRegistered = false;
+
+function App() {
+  return (
+    <div className="container">
+      <Form />
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+
+function Form() {
+  const [binario, setBinario] = useState("");
+  const [hexadecimal, setHexadecimal] = useState("");
+  const [decimal, setDecimal] = useState("");
+  const [selectedOption, setSelectedOption] = useState("Binario");
+
+  function handleOptionChange(e) {
+    setSelectedOption(e.target.value);
+  }
+
+  function converter() {
+    if (selectedOption === "Binario") {
+      binarioConverter();
+    } else if (selectedOption === "Hexadecimal") {
+      hexadecimalConverter();
+    }
+  }
+
+  function hexadecimalConverter() {
+    // Converte o número hexadecimal para decimal
+    const decimalValue = parseInt(hexadecimal, 16);
+
+    // Atualiza o estado do componente com o resultado
+    setDecimal(decimalValue);
+  }
+
+  function binarioConverter() {
+    if (!/^[01]+$/.test(binario)) {       // Verifica se a string binario contém apenas caracteres "0" ou "1" usando a expressão regular ^[01]+$.
+      setDecimal("Entrada inválida.");
+      return;
+    }
+
+    // Converter o número binário para decimal
+    var decimal = parseInt(binario, 2);
+
+    // Atualiza o estado do componente com o resultado
+    setDecimal(decimal);
+  }
+
+  return (
+    <form className="form">
+      <h1>Conversor Hexadecimal/Binário</h1>
+      <select value={selectedOption} onChange={handleOptionChange}>
+        <option value="Binario">Binário</option>
+        <option value="Hexadecimal">Hexadecimal</option>
+      </select>
+      {selectedOption === "Binario" ? (  // if ...
+        <input
+          type="text"
+          placeholder="Binário"
+          value={binario}
+          onChange={(e) => setBinario(e.target.value)}
+        />
+      ) : (                               // else ...
+        <input
+          type="text"
+          placeholder="Hexadecimal"
+          value={hexadecimal}
+          onChange={(e) => setHexadecimal(e.target.value)}
+        />
+      )}
+      <input type="text" placeholder="Decimal" value={decimal} readOnly />
+      <button type="button" onClick={converter}>
+        Converter
+      </button>
+    </form>
+  );
+}
+
+export default Form;
+
