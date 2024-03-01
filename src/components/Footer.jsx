3891d60894479c9d3d64423df8473e43345422d0
carolliie <anacvieira1415@gmail.com>
2024-03-01 14:39:30 -0300

    footer svg

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const currentYear = new Date().getFullYear();
 function Footer() {
     return (
         <footer>
-            <p>Copyright Carile © {currentYear}</p>
+            <p><img src="https://raw.githubusercontent.com/carolliie/web/9a5242fb29b610ee72dd335b097d3c9d943c81db/images/magician.svg" height="28" weight="28"/> © {currentYear} Carile, Inc</p>
         </footer>
     )  
 }
