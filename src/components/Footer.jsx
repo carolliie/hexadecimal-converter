0e2d97e3328f363a8a37b7530628de6fb4559a85
carolliie <anacvieira1415@gmail.com>
2024-03-02 16:18:30 -0300

    svg link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     return (
         <footer className="footer">
             <p>
-                <a href="https://github.com/carolliie" id="svg">
+                <a href="https://carolliie.github.io/web/" id="svg">
                 <img src="https://raw.githubusercontent.com/carolliie/web/9a5242fb29b610ee72dd335b097d3c9d943c81db/images/magician.svg" height="28" weight="28"/>
                 </a>        © {currentYear} Carile, Inc.
             </p>
